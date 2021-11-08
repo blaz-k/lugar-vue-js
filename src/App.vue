cc298a4acf18de84e3d920d5d6aaf899868fa027
blaz <blazyy@gmail.com>
2021-11-08 14:24:25 +0100

    Created navbar and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <Hero />
+  <About />
+  <Projects />
+  <Testimonial />
+  <Contact />
+  <Footer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import About from "./components/About.vue";
+import Projects from "./components/Projects.vue";
+import Testimonial from "./components/Testimonial.vue";
+import Contact from "./components/Contact.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+    Hero,
+    About,
+    Projects,
+    Testimonial,
+    Contact,
+    Footer,
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <!--ABOUT START-->
+  <div id="about-section" class="row">
+    <div class="col-md-6 about-left">
+      <img src="../assets/img/left-bg.png" alt="" class="img-fluid" />
+    </div>
+    <div class="col-md-6 about-right">
+      <h1>Award winning real estate company in Ljubljana</h1>
+      <h2>
+        Semper arcu mauris aliquam lacus. Massa erat vitae ultrices pharetra
+        scelerisque. Ipsum, turpis facilisis tempor pulvinar in lobortis ornare
+        magna.
+      </h2>
+    </div>
+  </div>
+  <!--ABOUT END-->
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style>
+</style>
